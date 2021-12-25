3bf28e47864751c55d9cc61890b633ea7663528c
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2021-12-25 23:19:11 +0530

    s

--- patterns/string-time -conversion-que.py
+++ patterns/string-time -conversion-que.py
@@ -5,5 +5,14 @@ print(v)
 print(s[:-2])
 
 import re
-text = 'The quick brown\nfox jumps*over the lazy dog.'
-print(re.split('; |, |\*|\n',text))
+text = 'The quick brown:fox jumps*over the lazy dog.'
+print(re.split(',|\*|\:',text))
+
+  if s[-2:] == "PM":
+        hours=str(int(s[:2])+12)
+        atime=s[:-2]
+        btime=atime[2:]
+        time=hours+btime
+    if s[-2:]=="AM":
+        time=s.lstrip('AM')
+    return(time)
