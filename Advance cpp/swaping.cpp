62bee1390a755fbfb9dfcab6c3feaa9562fd1800
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-09-10 11:45:08 +0530

    swaping strings

--- Advance cpp/swaping.cpp
+++ Advance cpp/swaping.cpp
@@ -8,5 +8,12 @@ int main() {
   swap(s, p);
   cout << "numbers after swaping are  s = " << s << " and p = " << p << endl;
 
+
+  string str1 = "sushant";
+  string str2 = "phalke";
+  cout << "strings before swaping are str1 = " << str1 << " and str2 = " << str2 << endl;
+  swap(str1, str2);
+  cout << "strings after swaping are  str1 = " << str1 << " and str2 = " << str2 << endl;
+
   return 0;
 }
