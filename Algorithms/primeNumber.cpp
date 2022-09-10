1552efff457ab746b41fedce8fef5b3bda118de8
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-09-10 11:27:02 +0530

    prime numbers

--- Algorithms/primeNumber.cpp
+++ Algorithms/primeNumber.cpp
@@ -12,23 +12,28 @@ int main() {
       cout << "number is not prime" << endl;
       flag = 1;
       break;
-    }
+    };
   }
   if (flag == 0) {
     cout << "number is prime" << endl;
-  }
+  };
 
-  string primestring;
-  for (int j = 0; j <= n; j++) {
-    bool flag = 0;
-    for (int i = 2; i <= sqrt(n); i++) {
-      if (n % i == 0) {
-        flag = 1;
+  string primestring = "";
+
+  for (int j = 2; j <= n; j++) {
+    bool flag2 = 0;
+    for (int k = 2; k <= sqrt(j); k++) {
+      if (j % k == 0) {
+        flag2 = 1;
         break;
       }
     }
-    if (flag == 0) {
+
+    if (flag2 == 0) {
+      primestring = primestring + "," + to_string(j);
     }
   }
+
+  cout << "Prime numbers up to " << n << " are : " << primestring << endl;
   return 0;
 }
