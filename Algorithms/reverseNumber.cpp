1de9d3fd8b1a80d94cdf58d8176a1daa15f82877
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-09-11 14:25:05 +0530

    reverse a number

---
+++ Algorithms/reverseNumber.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter a number to Reverse: ";
+  cin >> n;
+  int reverse = 0;
+  int lastDigit = 0;
+
+  while (n > 0) {
+    lastDigit = n % 10;
+    reverse = reverse * 10 + lastDigit;
+    n = n / 10;
+  }
+  cout << "reversed number is: " << reverse << endl;
+}
